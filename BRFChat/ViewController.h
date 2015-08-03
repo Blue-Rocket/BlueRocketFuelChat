9c644c2c334bad478b21d22095bafb06a5d0111a
Brian A. Hill <brian@pervasent.com>
2015-08-03 09:38:19 -0700

    Initial Commit

---
+++ BRFChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BRFChat
+//
+//  Created by Brian A. Hill on 8/3/15.
+//  Copyright (c) 2015 Blue Rocket. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
